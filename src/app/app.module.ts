5a123871e4b7357d95d3c4ddcb223146045783bf
Lowela A. Arevalo - BSIT 3C <89699060+arevalolowela@users.noreply.github.com>
2023-02-09 16:46:34 +0800

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes:Routes = [
+  {path:'',component:LoginComponent },
+  {path:'home',component:HomeComponent},
+  {path:'navbar',component:NavbarComponent}
+
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
